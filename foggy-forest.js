22c2ceb32f4debbe0edcf5bb94a41741bb4f35af
mealaud <mealaud@github.com>
2024-03-05 14:13:01 -0800

    blink

---
+++ foggy-forest.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#151515", "#ac4142", "#7e8e50", "#e5b567", "#6c99bb", "#9f4e85", "#7dd6cf", "#d0d0d0", "#505050", "#ac4142", "#7e8e50", "#e5b567", "#6c99bb", "#9f4e85", "#7dd6cf", "#f5f5f5"]);
+t.prefs_.set('foreground-color', "#ece6da");
+t.prefs_.set('background-color', "#1c1f2e");
+t.prefs_.set('cursor-color', '#6d7c72');
